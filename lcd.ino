eb7604b66881ba8d58c9483a8271349f0093ebc3
LXMKevince <31642335+LXMKevince@users.noreply.github.com>
2019-03-29 10:43:57 +0800

    添加文件

---
+++ lcd.ino
@@ -0,0 +1,32 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
+ 
+void setup() {
+lcd. begin(16, 2);
+}
+void loop()
+{
+digitalWrite(TrigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(TrigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite (TrigPin, LOW);
+ 
+ 
+int timer=-500;
+cm=pulseIn(EchoPin, HIGH)/58.0;
+cm=(int (cm*100.0))/100.0;
+delay(500);
+ 
+ 
+if(millis()>timer +500)
+{
+lcd. print ("Distancce:");
+lcd. setCursor(6,1);
+lcd. print (cm);
+lcd. print ("cm ");
+lcd. home();
+timer=millis()+500;
+}
+}
---
+++ lcd_Demo.ino
@@ -0,0 +1,21 @@
+#include <LiquidCrystal.h>
+
+ LiquidCrystal lcd(13,12,11,10,9,8,7);            //初始化LCD
+ void setup(){
+     lcd.begin(16,2);                               //设置LCD为16列*2行
+ }
+ void loop(){
+     lcd.clear();                                //清屏操作
+     lcd.setCursor(5,0);                            //设置光标位置为第一行，第六列
+     lcd.print("Welcome");                        //在LCD上输出信息
+     delay(10);                                    //延时程序
+     lcd.setCursor(2,1);
+     lcd.print("I am xinmin");
+     delay(3000);
+     lcd.clear();
+     lcd.setCursor(2,0);
+     lcd.print("Hello World!");
+     lcd.setCursor(0,1);
+     lcd.print("4-Bit-Interface");
+     delay(5000);
+ }
